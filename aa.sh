cd7167a98811d300e2b349ae6896c73c5e100641
Furkan Sahin <62509862+furkansahinz@users.noreply.github.com>
2022-04-19 12:16:34 +0300

    Add files via upload

---
+++ aa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+echo 
+
+#brave-browser https://www.youtube.com/watch?v=uNkU358uVow
+
+brave-browser lykzsx.com
+brave-browser github.com/furkansahinz
+brave-browser linkedin.com/in/furkansahinz
+
